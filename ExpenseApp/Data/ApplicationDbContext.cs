49cb975c342761c5407b693c451fdf2b89d42c35
Nguyen-Gray Khoa <khoanguyengray@gmail.com>
2022-05-19 15:08:37 -0700

    initila commit

---
+++ ExpenseApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ExpenseApp.Models;
+
+namespace ExpenseApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Account>().HasData(
+                new Models.Account { Id = 1, Depositer = "Stuart", CurrencyType = "Peso", Receiver = "Tony", TransactionType = "deposit", TransactionDescription = "Peso Cash Account", TransactionDate = DateTime.Now, Amount = 400 },
+                new Models.Account { Id = 2, Depositer = "", CurrencyType = "Peso", Receiver = "Tony", TransactionType = "withdrawal", TransactionDescription = "Casa 3 - Cleaning", TransactionDate = DateTime.Now, Amount = 400 },
+                new Models.Account { Id = 3, Depositer = "", CurrencyType = "Peso", Receiver = "Tony", TransactionType = "withdrawal", TransactionDescription = "Casa 3 - Cleaning", TransactionDate = DateTime.Now, Amount = 400 },
+                new Models.Account { Id = 4, Depositer = "Stuart", CurrencyType = "USD", Receiver = "", TransactionType = "deposit", TransactionDescription = "USD Cash Account", TransactionDate = DateTime.Now, Amount = 900 },
+                 new Models.Account { Id = 5, Depositer = "", CurrencyType = "USD", Receiver = "Tony", TransactionType = "withdrawal", TransactionDescription = "Monthly Casa Amigo Admin Fee", TransactionDate = DateTime.Now, Amount = 500 }
+                );
+        }
+        public DbSet<ExpenseApp.Models.Account>? Account { get; set; }
+    }
+}
---
+++ ExpenseApp/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace ExpenseApp.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string CurrencyType { get; set; } = "Peso";
+        public string? Depositer { get; set; } = "";
+
+        public string? Receiver { get; set; } = "";
+
+        public DateTime TransactionDate { get; set; } = DateTime.Today;
+
+        public string TransactionType { get; set; } = String.Empty;
+
+        public string TransactionDescription { get; set; } = String.Empty;
+
+        public float Amount { get; set; }
+
+    }
+}
